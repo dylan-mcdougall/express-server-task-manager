e9b2b41755297be778cbf0b35e1fde9d355eb16b
Dylan <dylan.7mcdougall@gmail.com>
2023-04-26 12:27:05 -0700

    established baseline modal popout

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Create functionality for the task interface
 // Add task         Sort options
 
-// const addTask = () => {
-//     let addButton = document.getElementById('add-task');
+window.addEventListener("DOMContentLoaded", () => {
+    let modal = document.getElementsByClassName('modal');
+    let closeButton = document.getElementById('close');
+    let addButton = document.getElementById('add-task');
 
-// }
+    addButton.addEventListener("click", () => {
+        modal[0].style.display = 'grid';
+    })
 
-
-// window.addEventListener("DOMContentLoaded", () => {
-    
-// })
+    closeButton.addEventListener('click', () => {
+        modal[0].style.display = 'none';
+    })
+})
