6cfa72f58da60805f0e5d536aa3b62903e3cf1e9
Dylan <dylan.7mcdougall@gmail.com>
2023-04-26 11:25:07 -0700

    created the basic framework for the task modal

---
+++ index.js
@@ -0,0 +1,12 @@
+// Create functionality for the task interface
+// Add task         Sort options
+
+const addTask = () => {
+    let addButton = document.getElementById('add-task');
+    
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    
+})
