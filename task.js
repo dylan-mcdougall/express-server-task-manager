07ea977afe4ef950d21ecaa06dc604ed161772f4
Dylan <dylan.7mcdougall@gmail.com>
2023-04-27 14:45:47 -0700

    established bare frame for task list section

--- task.js
+++ task.js
@@ -2,5 +2,5 @@ export function addTask(obj) {
     let taskList = document.querySelector('div.list-field > ul');
     let taskItem = document.createElement('li');
     
-    taskItem.id = taskList.children.length;
+    taskItem.innerHTML = ""
 }
