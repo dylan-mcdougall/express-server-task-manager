e9eea4152bffb088e85d014e5e7b4b097f6f2c96
Dylan <dylan.7mcdougall@gmail.com>
2023-04-26 21:52:02 -0700

    began work on creation of the task list item

---
+++ task.js
@@ -0,0 +1,6 @@
+export function addTask(obj) {
+    let taskList = document.querySelector('div.list-field > ul');
+    let taskItem = document.createElement('li');
+    
+    taskItem.id = taskList.children.length;
+}
